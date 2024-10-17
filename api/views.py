541692dd817a069c767d74921ff9c34fb6a3bc81
Yuri <Jolycougar@gmail.com>
2024-10-17 10:06:58 +0300

    edit: permission to view TaskList

--- api/views.py
+++ api/views.py
@@ -1,13 +1,17 @@
-from rest_framework import generics
+from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from tasks.models import Task
 from .filters import TaskFilter
 from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
-class TaskListView(generics.ListAPIView):
-    queryset = Task.objects.all()
+class TaskListView(ListAPIView):
     serializer_class = TaskSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = TaskFilter
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
 
