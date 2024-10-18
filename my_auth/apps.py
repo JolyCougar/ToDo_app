420ba88829107ce742bcc587cfe61958ffc0e645
Yuri <Jolycougar@gmail.com>
2024-10-18 21:07:26 +0300

    edit: func reset new pass in api

--- my_auth/apps.py
+++ my_auth/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class MyAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'my_auth'
+    verbose_name = 'Аккаунт'
 
     def ready(self):
         import my_auth.signals
