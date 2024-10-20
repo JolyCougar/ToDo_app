cad2776c7cb32e2009a7b335f0223fbdd1bf77aa
Yuri <Jolycougar@gmail.com>
2024-10-20 22:34:18 +0300

    fix: add schedule

--- my_auth/services.py
+++ my_auth/services.py
@@ -1,8 +1,10 @@
 from django.urls import reverse
-from .models import EmailVerification
+from .models import EmailVerification, Profile
 from .tasks import send_verification_email_task, send_new_password_email_task
 import random
 import string
+from django_celery_beat.models import PeriodicTask, IntervalSchedule, CrontabSchedule
+import json
 
 
 class EmailService:
@@ -31,3 +33,38 @@ class PasswordGenerator:
         """Генерация случайного пароля заданной длины."""
         characters = string.ascii_letters + string.digits + string.punctuation
         return ''.join(random.choice(characters) for _ in range(length))
+
+
+class TaskScheduler:
+    def __init__(self, profile):
+        self.profile = profile
+        self.task_name = f'delete_tasks_{self.profile.user.username}'
+
+    def schedule_deletion_tasks(self):
+        # Удалите старую задачу, если она существует
+        PeriodicTask.objects.filter(name=self.task_name).delete()
+
+        # Создайте новое расписание
+        schedule = self.get_schedule()
+        if schedule:
+            PeriodicTask.objects.create(
+                interval=schedule if isinstance(schedule, IntervalSchedule) else None,
+                crontab=schedule if isinstance(schedule, CrontabSchedule) else None,
+                name=self.task_name,
+                task='my_auth.tasks.delete_completed_tasks',
+                args=json.dumps([self.profile.user.id]),  # Передаем user_id в качестве аргумента
+            )
+
+    def get_schedule(self):
+        if self.profile.delete_frequency == 'minute':
+            return IntervalSchedule.objects.get_or_create(every=1, period=IntervalSchedule.MINUTES)[0]
+        elif self.profile.delete_frequency == 'hour':
+            return IntervalSchedule.objects.get_or_create(every=1, period=IntervalSchedule.HOURS)[0]
+        elif self.profile.delete_frequency == 'day':
+            return IntervalSchedule.objects.get_or_create(every=1, period=IntervalSchedule.DAYS)[0]
+        elif self.profile.delete_frequency == 'week':
+            return IntervalSchedule.objects.get_or_create(every=1, period=IntervalSchedule.WEEKS)[0]
+        elif self.profile.delete_frequency == 'month':
+            # Пример для месячного расписания: первое число каждого месяца
+            return CrontabSchedule.objects.get_or_create(minute=0, hour=0, day=1)[0]
+        return None
--- my_auth/tasks.py
+++ my_auth/tasks.py
@@ -3,6 +3,10 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from decouple import config
+from .models import Profile
+from tasks.models import Task
+import json
+from celery.schedules import crontab
 
 
 @shared_task
@@ -44,3 +48,10 @@ def send_new_password_email_task(user_email, new_password):
         fail_silently=False,
         html_message=html_message,  # Добавляем HTML-содержимое
     )
+
+
+@shared_task
+def delete_completed_tasks(user_id):
+    # Удалите все выполненные задачи для данного пользователя
+    Task.objects.filter(user_id=user_id, complete=True).delete()
+
