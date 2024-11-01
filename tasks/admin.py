16132ac5edee0d1110d83216810f06ebd5b9ca15
Yuri <Jolycougar@gmail.com>
2024-11-01 12:21:27 +0300

    edit: doc in task

--- tasks/admin.py
+++ tasks/admin.py
@@ -4,13 +4,35 @@ from .models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    """ Отоброжение модели задачи в админке """
+    """
+    Отоброжение модели задачи в админке
+
+    Отображение модели задачи в админке.
+
+    Этот класс настраивает отображение модели задачи в административном
+    интерфейсе Django. Он определяет, какие поля будут отображаться в
+    списке задач, а также позволяет выполнять поиск по определенным полям.
+
+    Атрибуты:
+        list_display (tuple): Поля, которые будут отображаться в списке задач.
+        list_display_links (tuple): Поля, по которым можно кликнуть для
+        перехода к редактированию задачи.
+        search_fields (tuple): Поля, по которым будет выполняться поиск
+        в списке задач.
+    """
 
     list_display = "pk", "name", "description_short", "complete"
     list_display_links = "pk", "name"
     search_fields = "name", "description", "user"
 
     def description_short(self, obj: Task) -> str:
+        """
+        Возвращает укороченное описание задачи для отображения в админке.
+
+        :param obj: Объект задачи.
+        :return: Укороченное описание задачи (до 48 символов) с добавлением
+                 многоточия, если описание длиннее.
+        """
         if len(obj.description) < 48:
             return obj.description
         return obj.description[:48] + "..."
--- tasks/tasks_mixins.py
+++ tasks/tasks_mixins.py
@@ -1,11 +1,29 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class EmailVerifiedMixin(LoginRequiredMixin):
-    """Mixin для проверки, подтверждена ли электронная почта пользователя."""
+    """
+    Mixin для проверки, подтверждена ли электронная почта пользователя.
 
-    def dispatch(self, request, *args, **kwargs):
+    Этот миксин используется для защиты представлений, требующих
+    подтверждения электронной почты. Если электронная почта
+    пользователя не подтверждена, он будет перенаправлен на
+    страницу с сообщением о необходимости подтверждения.
+    """
+
+    def dispatch(self, request, *args, **kwargs) -> HttpResponse:
+        """
+        Обрабатывает входящие запросы и проверяет, подтверждена ли
+        электронная почта пользователя.
+
+        :param request: HTTP запрос.
+        :return: HttpResponse с перенаправлением на страницу
+                 подтверждения электронной почты или ответом
+                 для дальнейшей обработки запроса.
+        """
+
         if request.user.is_authenticated:
             # Проверяем, подтверждена ли электронная почта
             if not request.user.profile.email_verified:
@@ -13,6 +31,14 @@ class EmailVerifiedMixin(LoginRequiredMixin):
                 return self.handle_email_not_verified(request)
         return super().dispatch(request, *args, **kwargs)
 
-    def handle_email_not_verified(self, request):
-        # Отображаем страницу с сообщением о необходимости подтверждения электронной почты
+    def handle_email_not_verified(self, request) -> HttpResponse:
+        """
+        Отображает страницу с сообщением о необходимости
+        подтверждения электронной почты.
+
+        :param request: HTTP запрос.
+        :return: HttpResponse с рендерингом страницы
+                 подтверждения электронной почты.
+        """
+
         return render(request, 'email_verification_required.html')
