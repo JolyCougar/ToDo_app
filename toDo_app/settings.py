84fbe2931fed9085186805b19f8a4dcfa2d6c1b1
Yuri <Jolycougar@gmail.com>
2024-11-11 14:53:22 +0300

    fix: edit dockerfile

--- toDo_app/settings.py
+++ toDo_app/settings.py
@@ -119,7 +119,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 SITE_ID = 1
 
